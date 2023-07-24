81eef782143f5594d4d098bb4d0433e33094bcc7
Bruno Michetti <brunomichetti@rootstrap.com>
2023-07-24 10:08:38 -0300

    ejer del 1 al 5

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,82 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-test('submitting the form calls onSubmit with username and password', () => {
+// extra 2, usar faker
+// function buildLoginForm() {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//   }
+// }
+
+// extra 3: usar overrides por si se quiere data especifica para overridear username o password
+// function buildLoginForm(overrides) {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     ...overrides,
+//   }
+// }
+
+// extra 4: usar test data bot
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
+  let submittedData
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
-  //
+  //const handleSubmit = data => (submittedData = data)
+
+  // extra 1: mockear funciones
+  const handleSubmit = jest.fn()
+
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
+  render(<Login onSubmit={handleSubmit} />)
   // 🐨 get the username and password fields via `getByLabelText`
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
-  //
+
+  // extra 2: usar faker
+  // extra 3: definir mypassword y pasarla
+  // const mypassword = 'mypassword'
+  // const {username, password} = buildLoginForm({password: mypassword})
+
+  // extra 4: usar test data bot
+  const {username, password} = buildLoginForm()
+  // await userEvent.type(usernameInput, 'test.user')
+  // await userEvent.type(passwordInput, 'test.user.password')
+
+  await userEvent.type(usernameInput, username)
+  await userEvent.type(passwordInput, password)
+
   // 🐨 click on the button with the text "Submit"
-  //
+  const submitButton = screen.getByText('Submit')
+  await userEvent.click(submitButton)
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  // expect(submittedData).toEqual({
+  //   username: username,
+  //   password: password,
+  // })
+
+  // extra 1: mockear funcion y usar tohavebeencalled
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,14 +3,20 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
 import Login from '../../components/login-submission'
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+// extra 1, importa los handlers definidos
+import {handlers} from 'test/server-handlers'
 
 const buildLoginForm = build({
   fields: {
+    // usa fake con una funcion, pero cada vez que se genera data, genera un username y password nuevo.
+    // no usa el mismo para todos los tests
     username: fake(f => f.internet.userName()),
     password: fake(f => f.internet.password()),
   },
@@ -24,9 +30,23 @@ const buildLoginForm = build({
 // )
 // you'll want to respond with an JSON object that has the username.
 // 📜 https://mswjs.io/
+// const server = setupServer(
+//   rest.post(
+//     'https://auth-provider.example.com/api/login',
+//     async (req, res, ctx) => res(ctx.json({username: req.body.username})),
+//   ),
+// )
+
+// extra 1, define el server reutilizando los handlers definidos en otra parte
+const server = setupServer(...handlers)
 
 // 🐨 before all the tests, start the server with `server.listen()`
+beforeAll(() => server.listen())
 // 🐨 after all the tests, stop the server with `server.close()`
+afterAll(() => server.close())
+
+// extra 4: hace reset para resetear entre tests las sobreescrituras de server.use
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -35,14 +55,70 @@ test(`logging in displays the user's username`, async () => {
   await userEvent.type(screen.getByLabelText(/username/i), username)
   await userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 uncomment this and you'll start making the request!
-  // await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
   // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
   // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
-
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
   // once the login is successful, then the loading spinner disappears and
   // we render the username.
   // 🐨 assert that the username is on the screen
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+// extra 2: unhappy paths
+// de forma analoga se puede probar sin username
+test(`logging in without password shows error`, async () => {
+  render(<Login />)
+
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+})
+
+// extra 3: tomar snapshot en el assertion al final
+test('omitting the password results in an error', async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  // don't type in the password
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
+})
+
+// extra 4: caso donde el server tira 500.
+// utiliza server.use It allows you to add server handlers after the server has already started.
+// And the server.resetHandlers() allows you to remove those added handlers between tests
+// to preserve test isolation and restore the original handlers.
+test('unknown server error displays the error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        // ctx es por context
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
